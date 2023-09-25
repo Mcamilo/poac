9bc5937f37f5d53860d41fd478fa45585e28be23
Mcamilo <matheuscmilo@gmail.com>
2023-09-25 11:36:52 +0200

    feat: add packages

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from datetime import datetime
 from surrogate.synthesize import Synthesize
 from surrogate.meta_features import MetaFeatures
+from surrogate.problem_space import ProblemSpace
 from surrogate.surrogate import Surrogate
 # from optimization.tpot import Tpot
 
-# TODO - Service to plot data
+# TODO - Service to plot/analize data
+# TODO - add data layers (raw/stage/processing/production)
 
 if __name__ == "__main__":
     print(f"Starting time: {datetime.now()}")
     try:
-        Synthesize()
-        # MetaFeatures()
+        Synthesize(sample_n=5)
+        MetaFeatures()
+        # ProblemSpace()
         # Surrogate()
         # TPOT()
     except Exception as e:
